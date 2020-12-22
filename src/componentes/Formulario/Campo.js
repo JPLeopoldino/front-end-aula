017d97988673c5506d5bb3cdb04d08ed180da6af
Joao Pedro <leopoldino26@gmail.com>
2020-12-22 14:26:54 -0300

    criando formulario de cadastro

---
+++ src/componentes/Formulario/Campo.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const campo = (props) => {
+    let input;
+
+    switch(props.especificacao.type){
+        case 'select':
+            input = <select {...props.especificacao.atributos}>
+                { props.especificacao.opcoes.map((opt, i)=>{
+                    return(<option key={opt.value + i} value={opt.value}>
+                            {opt.legend}
+                        </option>);
+                })}
+            </select>;
+            break;
+        default:
+            input = <input type="props.especificacao.type" {...props.especificacao.atributos} />;
+            break;
+    }
+
+    return(
+        <div>
+            <label htmlFor={props.especificacao.atributos.id}>{props.especificacao.label}: </label>
+            {input}
+        </div>
+    );
+}
+
+export default campo;
+
+
+
---
+++ src/componentes/Formulario/Formulario.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Campo from './Campo';
+
+const formulario = (props) => {
+    return(
+        <form>
+            <h1>{ props.info.titulo }</h1>
+            {
+                props.info.campos.map((campo, i)=>{
+                    return (
+                        <Campo key={i} especificacao={campo} />
+                    )
+                })
+            }
+        </form>
+    );
+}
+
+export default formulario;
